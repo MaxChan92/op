3b10faf1055494e54f799d83bfe185eec8c0362b
MaxChan92 <70808019+MaxChan92@users.noreply.github.com>
2023-01-05 09:06:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 echo 'src-git xiangfei https://github.com/xiangfeidexiaohuo/openwrt-packages' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore/tree/main/luci' >>feeds.conf.default
